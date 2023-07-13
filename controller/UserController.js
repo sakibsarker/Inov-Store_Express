fc62ff40d839a5a11b8616ab58a543559944d599
sakibsarker <sakib15-3824@diu.edu.bd>
2023-07-13 19:01:13 +0600

    jwt only cookie

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,9 +1,35 @@
 const asycHandler =require('../middleware/asyncHandler')
-const usersdata=require('../model/userModel');
+const Userdata=require('../model/userModel');
+const jwt=require('jsonwebtoken');
 
 //post and public access
 exports.authUser=asycHandler(async (req, res) => {
-  res.send('auth user')
+  const{email,password}=req.body;
+
+  const user=await Userdata.findOne({email})
+  if(user && (await user.matchPassword(password))){
+    const token=jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:'30d'});
+
+    //set jwt as HTTP only cookie
+    res.cookie('jwt',token,{
+      httpOnly:true,
+      secure:process.env.NODE_ENV!=='development',
+      sameSite:'strict',
+      maxAge:30*24*60*60*1000 //30 days
+    });
+
+    res.json({
+      _id:user._id,
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+    })
+  }
+  else{
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+
 });
 
 //post and public access
@@ -27,7 +53,7 @@ exports.updateUserProfile=asycHandler(async (req, res) => {
 
 //prive and get and admin
 exports.getAllUsers=asycHandler(async (req, res) => {
-    res.send('get user profile')
+    res.send('get all user')
   });
 
 //prive and get userby id
@@ -37,7 +63,7 @@ exports.getUsersById=asycHandler(async (req, res) => {
 
 //private and delete and admin delete user/:id
 exports.deleteUsers=asycHandler(async (req, res) => {
-    res.send('delete user profile')
+    res.send('delete users')
   });
 
 //private and put and admin update user/:id
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
-const jwt=require('jsonwebtoken');
-const bcrypt=require('bcryptjs');
 dotenv.config();
 const {notFound,errorHandler}=require('./middleware/errorMiddleware')
 const connectDB=require('./config/db');
@@ -12,6 +10,7 @@ connectDB();
 app.use(express.static('view'));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 //route import form another file
