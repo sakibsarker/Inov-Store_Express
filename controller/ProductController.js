53a9e6e69c0674e0b3d81373d8474ed4db318bdd
sakibsarker <sakib15-3824@diu.edu.bd>
2023-07-25 00:59:39 +0600

    admin create products

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -15,7 +15,21 @@ exports.getSingleProduct = asycHandler(async(req, res) => {
   else{
     res.status(404)
     throw new Error('resource not found')
-    // res.status(404).json({message:'Product not found'})
   }
+});
 
+exports.createProduct=asycHandler(async (req, res) => {
+  const product=new productsdata({
+    name:'Sample name',
+    price:0,
+    user:req.user._id,
+    image:'/images/airpods.jpg',
+    brand:'Sample Brand',
+    category: 'Sample category',
+    countInStock:0,
+    numReviews:0,
+    description:'Sample description',
+  })
+  const createProduct=await product.save();
+  res.status(200).json(createProduct);
 });
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllproducts,getSingleProduct} = require('../controller/ProductController');
+const {getAllproducts,getSingleProduct,createProduct} = require('../controller/ProductController');
 
 
-router.get('/',getAllproducts);
+const {protect,admin}=require('../middleware/authMiddleware');
+
+router.route('/').get(getAllproducts).post(protect,admin,createProduct);
 router.get('/:id',getSingleProduct);
 
 module.exports = router;
+
