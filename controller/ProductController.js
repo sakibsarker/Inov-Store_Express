0cf772692aa71b503d09de364917e04227d4d0de
sakibsarker <sakib15-3824@diu.edu.bd>
2023-07-26 15:03:31 +0600

    review create for user

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -73,3 +73,45 @@ exports.deleteProduct=asycHandler(async (req, res) => {
   }
   
 });
+
+
+//create review
+
+exports.createProductReview=asycHandler(async (req, res) => {
+  const {rating,comment}=req.body;
+
+  const product=await productsdata.findById(req.params.id)
+
+  if(product){
+    const alreadyReviewd=product.reviews.find(
+      (review)=>review.user.toString()===req.user._id.toString()
+    )
+
+    if(alreadyReviewd){
+      res.status(400);
+      throw new Error('Product already reviewed')
+    }
+
+    const review={
+      name:req.user.name,
+      rating:Number(rating),
+      comment,
+      user:req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews=product.reviews.length;
+
+    product.rating=product.reviews.reduce((acc,review)=>acc+review.rating,0)/product.reviews.length;
+
+    await product.save();
+    res.status(200).json({message:'Review Added'})
+    
+    
+  }else{
+    res.status(404);
+    throw new Error('Resource not found')
+  }
+  
+});
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllproducts,getSingleProduct,createProduct,updateProduct,deleteProduct} = require('../controller/ProductController');
+const {getAllproducts,
+    getSingleProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    createProductReview,
+} = require('../controller/ProductController');
 
 
 const {protect,admin}=require('../middleware/authMiddleware');
 
 router.route('/').get(getAllproducts).post(protect,admin,createProduct);
-router.route('/:id').get(getSingleProduct).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct);
+router.route('/:id').get(getSingleProduct)
+.put(protect,admin,updateProduct)
+.delete(protect,admin,deleteProduct);
+router.route('/:id/reviews').post(protect,createProductReview);
 
 module.exports = router;
 
